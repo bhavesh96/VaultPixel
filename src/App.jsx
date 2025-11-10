802a39f8305bb88a2be1149a425701445dd8bcd4
bhavesh96 <51844684+bhavesh96@users.noreply.github.com>
2025-11-09 22:34:27 -0800

    Fix import statement for Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import GameDetail from "./pages/GameDetail";
-import Home from "./pages/home";
+// import Home from "./pages/home";
 
 function App() {
   return (
